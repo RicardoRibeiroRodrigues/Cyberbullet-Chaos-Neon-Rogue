d993ee43187192685e749c33215118f9e766e8a0
RicardoRibeiroRodrigues <riywar@gmail.com>
2023-10-01 18:30:21 -0300

    Personagem andando + cenário basico

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private bool isMoving;
+    private Vector2 Movement;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        if (!isMoving)
+        {
+            if (Movement != Vector2.zero)
+            {
+                // Rotate the player to face the direction of movement.
+                if (Movement.x < 0)
+                {
+                    transform.rotation = Quaternion.Euler(0, 180, 0);
+                } else
+                {
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                }
+                var targetPos = transform.position;
+                targetPos.x += Movement.x;
+                targetPos.y += Movement.y;
+                StartCoroutine(Move(targetPos));
+            }
+        }
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+
+    void OnMove(InputValue value)
+    {
+        Movement = value.Get<Vector2>();
+    }
+}
