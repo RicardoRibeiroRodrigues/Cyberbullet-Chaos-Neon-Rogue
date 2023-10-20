a347d7093a6d18823c880bc251db8dc67b767e7c
FelixLuciano <felix.lucianodias@gmail.com>
2023-10-20 11:04:16 -0300

    feat: selection cursor

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -5,24 +5,23 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.TextCore.Text;
+using Unity.VisualScripting;
 
-public class GameController : MonoBehaviour
+public class MainMenuController : MonoBehaviour
 {
     public state State = state.HomeScreen;
     public GameObject Camera;
     public float CameraSmoothTime = 1.0f;
     public Canvas canvas;
-    public float CharacterSpeed = 1.0f;
     public GameObject Character1;
     public GameObject Character2;
     public GameObject Character3;
+    public GameObject CharacterCursor;
+    public GameObject WeaponCursor;
 
     private Vector3 CameraTarget = Vector3.zero;
     private Vector3 CameraVelocity = Vector3.zero;
-    private SpriteRenderer CharacterRenderer;
-    private Vector3 CharacterTarget = Vector3.zero;
-    private Vector3 CharacterVelocity = Vector3.zero;
-    private GameObject SelectedCharacter;
+    private MainMenuCharacterController SelectedCharacterController;
 
     public enum state
     {
@@ -41,28 +40,47 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        Camera.transform.position = Vector3.SmoothDamp(Camera.transform.position, CameraTarget, ref CameraVelocity, CameraSmoothTime, CharacterSpeed);
-        SelectedCharacter.transform.position = Vector3.SmoothDamp(SelectedCharacter.transform.position, CharacterTarget, ref CharacterVelocity, 0.0f, CharacterSpeed);
-        CharacterRenderer.flipX = CharacterVelocity.x < 0.0;
+        Camera.transform.position = Vector3.SmoothDamp(Camera.transform.position, CameraTarget, ref CameraVelocity, CameraSmoothTime);
     }
 
     public void SelectCharacter(int characterIndex)
     {
+        float cursorPositionY = 0.0f;
+
         switch (characterIndex)
         {
             case 0:
-                SelectedCharacter = Character1;
+                SelectedCharacterController = Character1.GetComponent<MainMenuCharacterController>();
+                cursorPositionY = 2.98f;
                 break;
             case 1:
-                SelectedCharacter = Character2;
+                SelectedCharacterController = Character2.GetComponent<MainMenuCharacterController>();
+                cursorPositionY = -0.08f;
                 break;
             case 2:
-                SelectedCharacter = Character3;
+                SelectedCharacterController = Character3.GetComponent<MainMenuCharacterController>();
+                cursorPositionY = -3.08f;
                 break;
         }
 
-        CharacterRenderer = SelectedCharacter.GetComponent<SpriteRenderer>();
-        CharacterTarget = SelectedCharacter.transform.position;
+        CharacterCursor.transform.position = new Vector3(CharacterCursor.transform.position.x, cursorPositionY, CharacterCursor.transform.position.z);
+    }
+
+    public void SelectWeapon(int weaponIndex)
+    {
+        float cursorPositionY = 0.0f;
+
+        switch (weaponIndex)
+        {
+            case 0:
+                cursorPositionY = 1.985f;
+                break;
+            case 1:
+                cursorPositionY = -2.015f;
+                break;
+        }
+
+        WeaponCursor.transform.position = new Vector3(WeaponCursor.transform.position.x, cursorPositionY, WeaponCursor.transform.position.z);
     }
 
     public void UpdateState(int stateIndex)
@@ -92,12 +110,12 @@ public class GameController : MonoBehaviour
     void HandleCharacterScreen()
     {
         CameraTarget = new Vector3(16, 0, -10);
-        CharacterTarget.x = 11;
+        SelectedCharacterController.Target.x = 11;
     }
 
     void HandleWeaponScreen()
     {
         CameraTarget = new Vector3(32, 0, -10);
-        CharacterTarget.x = 30;
+        SelectedCharacterController.Target.x = 30;
     }
 }
